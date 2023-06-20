867f5050c858dcfbae624218a644dfc506ad30df
AnthonyJeria <anth.jeria@duocuc.cl>
2023-06-20 17:42:25 -0400

    db sqlite y usuario admin creado

--- noticias/admin.py
+++ noticias/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Tipo_noticia, Noticia, Producto
 
 # Register your models here.
+
+admin.site.register(Tipo_noticia)
+admin.site.register(Noticia)
+admin.site.register(Producto)
--- noticias/models.py
+++ noticias/models.py
@@ -2,19 +2,28 @@ from django.db import models
 
 # Create your models here.
 
+class Tipo_noticia(models.Model):
+    id_tipo                 =models.AutoField(db_column='idTipo', primary_key=True)
+    nombre_tipo_noticia     =models.CharField(max_length=50)
+
+    def __str__(self):
+        return str(self.nombre_tipo_noticia)
+
 class Noticia(models.Model):
-    id_noticia      =models.AutoField(db_column='idNoticia', primary_key=True)
-    titular         =models.CharField(max_length=500)
-    subtitulo       =models.CharField(max_length=500)
-    entradilla      =models.CharField(max_length=500)
-    cuerpo_noticia  =models.CharField(max_length=10000)
-    fecha_noticia   =models.DateField(blank=False, null=False)
-    imagen_noticia  =models.ImageField(upload_to="noticias", null=True)
+    id_noticia              =models.AutoField(db_column='idNoticia', primary_key=True)
+    titular                 =models.CharField(max_length=500)
+    subtitulo               =models.CharField(max_length=500)
+    entradilla              =models.CharField(max_length=5000)
+    cuerpo_noticia          =models.CharField(max_length=10000)
+    fecha_noticia           =models.DateField(blank=False, null=False)
+    imagen_noticia          =models.ImageField(upload_to="noticias", null=True)
+    id_tipo                 =models.ForeignKey('Tipo_noticia' ,on_delete=models.CASCADE, db_column='id_tipo')
+    num_subtipo_noticia     =models.IntegerField()
 
     def __str__(self):
         return str(self.titular)
     
-class producto(models.Model):
+class Producto(models.Model):
     id_producto     =models.AutoField(db_column='idProducto', primary_key=True)
     nombre_producto =models.CharField(max_length=50)
     precio          =models.IntegerField()
